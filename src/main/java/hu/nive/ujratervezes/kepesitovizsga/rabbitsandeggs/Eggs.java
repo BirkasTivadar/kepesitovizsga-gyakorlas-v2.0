1973fe5c4ebf73a80c46cc7d580df2a98c78c179
BirkasTivadar <birkastivadar@gmail.com>
2021-04-25 16:07:32 +0200

    Eggs kész

--- src/main/java/hu/nive/ujratervezes/kepesitovizsga/rabbitsandeggs/Eggs.java
+++ src/main/java/hu/nive/ujratervezes/kepesitovizsga/rabbitsandeggs/Eggs.java
@@ -1,5 +1,30 @@
 package hu.nive.ujratervezes.kepesitovizsga.rabbitsandeggs;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Eggs {
 
+
+    public Rabbit getRabbitWithMaxEggs() {
+
+        String name = null;
+        int max = 0;
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(
+                Eggs.class.getResourceAsStream("/eggs.csv")))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                int temp = Integer.parseInt(line.split(";")[1]);
+                if (temp > max) {
+                    name = line.split(";")[0];
+                    max = temp;
+                }
+            }
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can not read file", ioe);
+        }
+        return new Rabbit(name, max);
+    }
 }
--- src/test/java/hu/nive/ujratervezes/kepesitovizsga/rabbitsandeggs/EggsTest.java
+++ src/test/java/hu/nive/ujratervezes/kepesitovizsga/rabbitsandeggs/EggsTest.java
@@ -1,15 +1,17 @@
 package hu.nive.ujratervezes.kepesitovizsga.rabbitsandeggs;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EggsTest {
 
-//    @Test
-//    public void testGetRabbitWithMaxEggs() {
-//        Rabbit rabbit = new Eggs().getRabbitWithMaxEggs();
-//
-//        Assertions.assertEquals("Nyunyi", rabbit.getName());
-//        Assertions.assertEquals(32, rabbit.getEggs());
-//    }
+    @Test
+    void testGetRabbitWithMaxEggs() {
+        Rabbit rabbit = new Eggs().getRabbitWithMaxEggs();
+
+        assertEquals("Nyunyi", rabbit.getName());
+        assertEquals(32, rabbit.getEggs());
+    }
 
 }
