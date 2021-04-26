856f5fd4b636ebbead2ae5f6d453acd2f5836543
BirkasTivadar <birkastivadar@gmail.com>
2021-04-26 05:24:36 +0200

    Dictionary kész

--- src/main/java/hu/nive/ujratervezes/kepesitovizsga/dictionary/Dictionary.java
+++ src/main/java/hu/nive/ujratervezes/kepesitovizsga/dictionary/Dictionary.java
@@ -1,6 +1,81 @@
 package hu.nive.ujratervezes.kepesitovizsga.dictionary;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Dictionary {
 
+    private DataSource dataSource;
 
+    public Dictionary(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public String getEnglishWord(String hungarianWord) {
+        String result;
+
+        try (
+                Connection conn = dataSource.getConnection();
+                PreparedStatement ps = conn.prepareStatement("SELECT angol FROM words WHERE magyar = ?;")
+        ) {
+
+            ps.setString(1, hungarianWord);
+
+            result = getStringEnglish(ps);
+
+        } catch (SQLException sqlException) {
+            throw new IllegalStateException("Connection failed", sqlException);
+        }
+        return result;
+    }
+
+    private String getStringEnglish(PreparedStatement ps) {
+
+        try (ResultSet rs = ps.executeQuery()) {
+
+            if (rs.next()) {
+                return rs.getString("angol");
+            }
+
+        } catch (SQLException sqlException) {
+            throw new IllegalStateException("Cannot query", sqlException);
+        }
+        throw new IllegalArgumentException("No such word in dictionary.");
+    }
+
+    public String getHungarianWord(String englishWord) {
+        String result;
+
+        try (
+                Connection conn = dataSource.getConnection();
+                PreparedStatement ps = conn.prepareStatement("SELECT magyar FROM words WHERE angol = ?;")
+        ) {
+
+            ps.setString(1, englishWord);
+
+            result = getStringHungarian(ps);
+
+        } catch (SQLException sqlException) {
+            throw new IllegalStateException("Connection failed", sqlException);
+        }
+        return result;
+    }
+
+    private String getStringHungarian(PreparedStatement ps) {
+
+        try (ResultSet rs = ps.executeQuery()) {
+
+            if (rs.next()) {
+                return rs.getString("magyar");
+            }
+
+        } catch (SQLException sqlException) {
+            throw new IllegalStateException("Cannot query", sqlException);
+        }
+        throw new IllegalArgumentException("No such word in dictionary.");
+    }
 }
+
--- src/test/java/hu/nive/ujratervezes/kepesitovizsga/dictionary/DictionaryTest.java
+++ src/test/java/hu/nive/ujratervezes/kepesitovizsga/dictionary/DictionaryTest.java
@@ -1,50 +1,50 @@
 package hu.nive.ujratervezes.kepesitovizsga.dictionary;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
+import org.flywaydb.core.Flyway;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DictionaryTest {
 
-//    private Dictionary dictionary;
-//    private MariaDbDataSource dataSource;
-//
-//    @BeforeEach
-//    public void setUp() {
-//        try {
-//            dataSource = new MariaDbDataSource();
-//            dataSource.setUrl("jdbc:mariadb://localhost:3306/employees?useUnicode=true");
-//            dataSource.setUser("employees");
-//            dataSource.setPassword("employees");
-//        } catch (SQLException sqle) {
-//            throw new IllegalStateException("Can not get data from database.", sqle);
-//        }
-//
-//        dictionary = new Dictionary(dataSource);
-//
-//        Flyway fw = Flyway.configure().dataSource(dataSource).load();
-//        fw.clean();
-//        fw.migrate();
-//    }
-//
-//    @Test
-//    public void getEnglishWord() {
-//        Assertions.assertEquals("chair", dictionary.getEnglishWord("szék"));
-//    }
-//
-//    @Test
-//    public void getHungarianWord() {
-//        Assertions.assertEquals("kanál", dictionary.getHungarianWord("spoon"));
-//    }
-//
-//    @Test
-//    public void testNotExistingWord() {
-//        Exception ex = Assertions.assertThrows(IllegalArgumentException.class, () -> dictionary.getEnglishWord("kerítés"));
-//        Assertions.assertEquals("No such word in dictionary.", ex.getMessage());
-//    }
-//
-//    @Test
-//    public void testNotExistingWord2() {
-//        Exception ex = Assertions.assertThrows(IllegalArgumentException.class, () -> dictionary.getHungarianWord("apple"));
-//        Assertions.assertEquals("No such word in dictionary.", ex.getMessage());
-//    }
+    private Dictionary dictionary;
+    private MysqlDataSource dataSource;
 
+    @BeforeEach
+    public void setUp() {
+        dataSource = new MysqlDataSource();
+        dataSource.setUrl("jdbc:mysql://localhost:3306/employees?useUnicode=true");
+        dataSource.setUser("employees");
+        dataSource.setPassword("employees");
+
+        dictionary = new Dictionary(dataSource);
+
+        Flyway fw = Flyway.configure().dataSource(dataSource).load();
+        fw.clean();
+        fw.migrate();
+    }
+
+    @Test
+    public void getEnglishWord() {
+        assertEquals("chair", dictionary.getEnglishWord("szék"));
+    }
+
+    @Test
+    public void getHungarianWord() {
+        assertEquals("kanál", dictionary.getHungarianWord("spoon"));
+    }
+
+    @Test
+    public void testNotExistingWord() {
+        Exception ex = assertThrows(IllegalArgumentException.class, () -> dictionary.getEnglishWord("kerítés"));
+        assertEquals("No such word in dictionary.", ex.getMessage());
+    }
+
+    @Test
+    public void testNotExistingWord2() {
+        Exception ex = assertThrows(IllegalArgumentException.class, () -> dictionary.getHungarianWord("apple"));
+        assertEquals("No such word in dictionary.", ex.getMessage());
+    }
 }
